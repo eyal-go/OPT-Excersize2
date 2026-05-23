0c793055a20beee42600103d545ed3a5ecd8ea40
Eyal Goldberg <eyalg7@gmail.com>
2026-05-23 18:08:21 +0300

    initial commit

---
+++ Section2.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def BwdSub(U: np.array, b: np.array) -> np.array:
+#Assume U is square upper triangular with no 0s on the diagonal
+
+    n = U.shape[0]
+    x = np.zeros(n)
+    for i in range(n-1, -1, -1):
+        row_sum = 0
+        for j in range(i+1, n, 1):
+            row_sum += U[i,j]*x[j]
+        x[i] = (b[i] - row_sum)/U[i, i]
+    
+    return x
+
+
+def FwdSub(L: np.array, b: np.array) -> np.array:
+#Assume L is square lower triangular with no 0s on the diagonal
+
+    n = L.shape[0]
+    x = np.zeros(n)
+    for i in range(0, n, 1):
+        row_sum = 0
+        for j in range(0, i, 1):
+            row_sum += L[i,j]*x[j]
+        x[i] = (b[i] - row_sum)/L[i,i]
+
+    return x
+
+
+A = np.array([[2, 1, 2], [1, -2, 1], [1, 2, 3], [1, 1, 1]])
+b = np.array([6,1,5,2])
+print(A)
+AT = np.transpose(A)
+ATA = np.linalg.matmul(AT, A)
+L = np.linalg.cholesky(ATA)
+
+########## b ##########
+#We want to solve ATAx = ATb.
+#We have ATA = LLT.
+#Essentially, we will treat ATb as the new vector to solve.
+
+c_b = np.matmul(AT, b)
+
+#Now we have LLTx = c. first we'll solve Ly = b using FwdSub:
+y_b = FwdSub(L = L, b = c_b)
+
+#Next we will solve Ux = y, with U = LT with BwdSub to get the LS solution:
+x_b = BwdSub(U = np.transpose(L), b = y_b)
+
+print(x_b)
+
+########## c ##########
+#Using QR factorization, we saw in class that the LS solution is given by x = R^-1QTb.
+
+Q, R = np.linalg.qr(A)
+
+#First, we'll calculate QTb:
+
+QTb = np.matmul(np.transpose(Q), b)
+
+#Can we use built in inverse?
+#Next, since R is upper traingular and full rank, we can calculate it's inverse using BwdSub for each column:
+for i in range(0, 4, 1):
