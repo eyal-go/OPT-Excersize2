0e6cc36e00ba9c0b172935b9067d0ae34a0641f5
Eyal Goldberg <eyalg7@gmail.com>
2026-05-24 22:04:57 +0300

    MGS copied

--- Section3.py
+++ Section3.py
@@ -18,11 +18,20 @@ def GMQR(A: np.array) -> tuple[np.array, np.array]:
     Q = np.column_stack(Q_list)
     return Q, R    
 
-A = np.array([[2, 1, 2], [1, -2, 1], [1, 2, 3], [1, 1, 1]])
-Q, R = GMQR(A)
+def MGMQR(A: np.array) -> tuple[np.array, np.array]:
+    a1 = A[:, 0]
+    R = np.zeros((A.shape[1], A.shape[1]))
+    R[0, 0] = np.linalg.norm(a1)
+    q1 = a1/R[0, 0]
+    Q_list = [q1]
+    for i in range(1, A.shape[1], 1):
+        qi = A[:, i]
+        for j in range(0, i, 1):
+            R[j, i] = np.dot(Q_list[j], qi)
+            qi = qi - R[j, i]*Q_list[j]
+        R[i, i] = np.linalg.norm(qi)
+        qi = qi/R[i, i]
+        Q_list.append(qi)
 
-print(Q, R)
-
-C, V = np.linalg.qr(A)
-
-print(C, V)
+    Q = np.column_stack(Q_list)
+    return Q, R    
